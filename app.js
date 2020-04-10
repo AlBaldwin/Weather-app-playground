02f077e8eb88bc3bfc344d9c73283367cbf201e8
Alan Baldwin <baldwingaming1979@gmail.com>
2020-04-10 21:52:29 +0100

    Connect to the MapBox API

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const config = require('./config');
 const request = require('request')
 
-const url = `${config.app.url}/current?access_key=${config.app.key}&query=37.8267,-122.4233`  
+const wsRrl = `${config.weatherStack.url}/current?access_key=${config.weatherStack.key}&query=37.8267,-122.4233&units=f`  
+const mbUrl = `${config.mapBox.url}/geocoding/v5/mapbox.places/London.json?access_token=${config.mapBox.key}`
 
-request({ url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: wsRrl, json: true}, (error, response) => {
+    const temp = response.body.current.temperature
+    const precip = response.body.current.precip
+    const desc = response.body.current.weather_descriptions[0]
+
+    console.log(desc +' Its currently ' + temp + ' Fahrenheit and has ' + precip + ' chance of rain')
+})
+
+request( {url: mbUrl, json: true}, (error, response) =>{
+    const latitude = response.body.features[0].geometry.coordinates[0]
+    const longtitude = response.body.features[0].geometry.coordinates[1]
+    console.log('For London the Latitude is ' + latitude + ' and longtitude is ' + longtitude)
 })
