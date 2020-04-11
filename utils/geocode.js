3f5ff50ac82e101565cbc387607238cf95b77e73
Alan Baldwin <baldwingaming1979@gmail.com>
2020-04-11 11:07:46 +0100

    Refactor/added a call back function for geocode

---
+++ utils/geocode.js
@@ -0,0 +1,22 @@
+const config = require('../config');
+const request = require('request')
+
+const geocode = (address, callback) => {
+    
+    const mbUrl = `${config.mapBox.url}/geocoding/v5/mapbox.places/` + encodeURIComponent(address) + `.json?access_token=${config.mapBox.key}`
+    request ({ url: mbUrl, json: true}, (error, response) =>{
+        if (error) {
+            callback('Uable to connect to mapBox services', undefined)
+        } else if (response.body.features.length === 0) {
+            callback('Unable to find location. Try another search', undefined)
+        } else {
+            callback(undefined, {
+                latitude: response.body.features[0].geometry.coordinates[0],
+                longtitide: response.body.features[0].geometry.coordinates[1],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
